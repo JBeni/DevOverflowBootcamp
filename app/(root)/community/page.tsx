9eb0b78e3e424180fd6e5e22241e903c05d8e965
Jîtcă Beniamin <JBeni@users.noreply.github.com>
2024-04-07 10:51:58 +0100

    fix issues with SearchBar import

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,6 +1,6 @@
 import UserCard from '@/components/cards/UserCard';
 import Filter from '@/components/shared/Filter';
-import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import LocalSearchbar from '@/components/shared/search/LocalSearchBar';
 import { UserFilters } from '@/constants/filters';
 import { getAllUsers } from '@/lib/actions/user.action';
 import Link from 'next/link';
--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,7 +1,7 @@
 import UserCard from '@/components/cards/UserCard';
 import Filter from '@/components/shared/Filter';
 import NoResult from '@/components/shared/NoResult';
-import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import LocalSearchbar from '@/components/shared/search/LocalSearchBar';
 import { UserFilters } from '@/constants/filters';
 import { getAllTags } from '@/lib/actions/tag.action';
 import Link from 'next/link';
